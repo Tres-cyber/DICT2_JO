4d125f7d373f6088c9349c022a4cbc0f8e2ce268
wabbit12 <107483377+wabbit12@users.noreply.github.com>
2024-06-27 14:52:52 +0800

    Create 03-added-projects.sql

---
+++ database/03-added-projects.sql
@@ -0,0 +1,47 @@
+INSERT INTO `projects`(
+    `project_id`,
+    `project_name`,
+    `project_code`,
+    `focal_person_id`
+)
+VALUES(
+    NULL,
+    'ICT Industry Development Bureau',
+    'IIDB',
+    '26'
+),(
+    NULL,
+    'National ICT Planning, Policy and Standards Bureau',
+    'NIPPSB',
+    '6'
+),(
+    NULL,
+    'electronic Local Government Unit',
+    'e-LGU',
+    '18'
+),(NULL, 'Cybersecurity', 'PNPKI', '35'),(
+    NULL,
+    'National Government Portal',
+    'NGP',
+    '26'
+),(
+    NULL,
+    'ICT Literacy and Competency Development Bureau',
+    'ILCDB',
+    '6'
+),(
+    NULL,
+    'Government Emergency Communications System',
+    'GECS',
+    '18'
+),(
+    NULL,
+    'Free Wi-Fi for All - Free Public Internet Access Program',
+    'Free Wi-Fi 4 All',
+    '47'
+),(
+    NULL,
+    'Government Network',
+    'GovNet',
+    '47'
+);
