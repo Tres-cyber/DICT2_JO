5d2e400011d4605517c2feec17b6697f73b8daf4
Tres-cyber <louiegalamaythe3rd@gmail.com>
2024-07-01 20:55:19 +0800

    Added login

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
     <link rel="icon" href="<%= BASE_URL %>favicon.ico">
     <title><%= htmlWebpackPlugin.options.title %></title>
   </head>
@@ -12,9 +13,26 @@
       <strong>We're sorry but <%= htmlWebpackPlugin.options.title %> doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
     </noscript>
     <div id="app">
-<counter-button></counter-button>
-<counter-button></counter-button>
-</div>
-    <!-- built files will be auto injected -->
+       <div class="login-container">
+        <div class="login-logo-wrapper">
+          <img src="assets/logo.png" alt="" class="login-logo">
+        </div>
+       <div class="login-wrapper-2">
+       <div class="login-box">
+          <h1 class="login-title">login</h1>
+          <form action="">
+            <label class="input-title" for="">Email</label>
+            <input class="input-wrapper" type="text">
+            <label class="input-title" for="">Password</label>
+            <input class="input-wrapper" type="text">
+            <div class="input-submit-wrapper">
+            <input class="input-submit" type="submit" value="Submit">
+            </div>
+          </form>
+        </div>
+       </div>
+       </div>
+      </div>
+        
   </body>
 </html>
