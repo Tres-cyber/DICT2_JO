2f601722c32dc349f303ca022451d1801af522da
JehhB <jwy.bayod@gmail.com>
2024-07-06 20:16:17 +0800

    Added drafting

--- public/app/setup.php
+++ public/app/setup.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 require_once __DIR__ . '/database.php';
@@ -9,8 +10,10 @@ session_start();
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/views');
 $twig = new \Twig\Environment($loader, [
+  'debug' => \Dict\Jo\ViteUtil::isDev(),
   'autoescape' => 'html',
 ]);
 
 $twig->addGlobal('session', $_SESSION);
+if (\Dict\Jo\ViteUtil::isDev()) $twig->addExtension(new \Twig\Extension\DebugExtension());
 $twig->addFunction(new \Twig\TwigFunction('vite', [\Dict\Jo\ViteUtil::class, 'vite']));
