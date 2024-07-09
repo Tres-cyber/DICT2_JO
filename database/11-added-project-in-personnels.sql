05fe502b8ec99ee569bfdf95f49f14e6f362df0e
wabbit12 <rectocharlsdave@gmail.com>
2024-07-09 11:10:50 +0800

    added-sql

---
+++ database/11-added-project-in-personnels.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Personnels
+ADD project_id INTEGER UNSIGNED NOT NULL DEFAULT 3;
+
+ALTER TABLE Personnels
+ADD CONSTRAINT fk_project_id FOREIGN KEY (project_id)
+    REFERENCES Projects (project_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
