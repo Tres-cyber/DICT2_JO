c5069625ef5d7f61ada38900aec3058cdc3f4c82
JehhB <jwy.bayod@gmail.com>
2024-07-11 11:50:11 +0800

    Removed logos from storage

--- public/admin/edit_project.php
+++ public/admin/edit_project.php
@@ -4,29 +4,29 @@ $account = protectRoute(true);
 
 // Check if it's a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if 'id' is set in POST data
-    if (isset($_POST['id'])) {
+  // Check if 'id' is set in POST data
+  if (isset($_POST['id'])) {
 
-        
-        $project_logo_path = ""; 
-        if (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] === UPLOAD_ERR_OK) {
-            $uploadDir = __DIR__ . '/../storage/';
-            $target_name = uniqid() . basename($_FILES['project_logo']['name']);
-            $uploadFile = $uploadDir . $target_name;
 
-            if (move_uploaded_file($_FILES['project_logo']['tmp_name'], $uploadFile)) {
-                $project_logo_path = '/storage/'. $target_name;
-            } else {
-                echo "File upload failed!";
-                exit();
-            }
-        } elseif (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] !== UPLOAD_ERR_NO_FILE) {
-            echo "File upload error occurred.";
-            exit();
-        }
+    $project_logo_path = "";
+    if (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] === UPLOAD_ERR_OK) {
+      $uploadDir = __DIR__ . '/../storage/logo/';
+      $target_name = uniqid() . basename($_FILES['project_logo']['name']);
+      $uploadFile = $uploadDir . $target_name;
 
-        // Construct the SQL statement
-        $sql = "
+      if (move_uploaded_file($_FILES['project_logo']['tmp_name'], $uploadFile)) {
+        $project_logo_path = '/storage/logo/' . $target_name;
+      } else {
+        echo "File upload failed!";
+        exit();
+      }
+    } elseif (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] !== UPLOAD_ERR_NO_FILE) {
+      echo "File upload error occurred.";
+      exit();
+    }
+
+    // Construct the SQL statement
+    $sql = "
             UPDATE Projects 
             SET 
                 project_name = :project_name, 
@@ -37,34 +37,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 project_id = :project_id
         ";
 
-        // Get focal_person_id from Personnels table
-        $focal_person_id = execute("SELECT personnel_id FROM Personnels WHERE name = :name", ['name' => $_POST['focal_person']])->fetchColumn();
-        if (!$focal_person_id) {
-            $focal_person_id = null; // Set to null if no matching personnel found
-        }
-
-        // Prepare the arguments for the SQL query
-        $args = [
-            ':project_name' => $_POST['project_name'],
-            ':project_code' => $_POST['project_code'],
-            ':focal_person_id' => $focal_person_id,
-            ':project_logo' => $project_logo_path, // Set project_logo to null if no file was uploaded
-            ':project_id' => $_POST['id']
-        ];
-
-        // Execute the SQL query
-        execute($sql, $args);
-
-        // Redirect to the projects page after update
-        header('Location: /admin/projects.php');
-        exit();
-    } else {
-        echo "Project ID is required!";
-        exit();
+    // Get focal_person_id from Personnels table
+    $focal_person_id = execute("SELECT personnel_id FROM Personnels WHERE name = :name", ['name' => $_POST['focal_person']])->fetchColumn();
+    if (!$focal_person_id) {
+      $focal_person_id = null; // Set to null if no matching personnel found
     }
+
+    // Prepare the arguments for the SQL query
+    $args = [
+      ':project_name' => $_POST['project_name'],
+      ':project_code' => $_POST['project_code'],
+      ':focal_person_id' => $focal_person_id,
+      ':project_logo' => $project_logo_path, // Set project_logo to null if no file was uploaded
+      ':project_id' => $_POST['id']
+    ];
+
+    // Execute the SQL query
+    execute($sql, $args);
+
+    // Redirect to the projects page after update
+    header('Location: /admin/projects.php');
+    exit();
+  } else {
+    echo "Project ID is required!";
+    exit();
+  }
 }
 
 // Redirect to projects page if not a POST request
 header('Location: /admin/projects.php');
 exit();
-?>
--- public/admin/projects.php
+++ public/admin/projects.php
@@ -4,47 +4,46 @@ require_once __DIR__ . '/../app/setup.php';
 $account = protectRoute(true);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $project_logo_path = ""; 
-    if (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] === UPLOAD_ERR_OK) {
-        $uploadDir = __DIR__ . '/../storage/';
-        $target_name = uniqid() . basename($_FILES['project_logo']['name']);
-        $uploadFile = $uploadDir . $target_name;
+  $project_logo_path = "";
+  if (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] === UPLOAD_ERR_OK) {
+    $uploadDir = __DIR__ . '/../storage/logo/';
+    $target_name = uniqid() . basename($_FILES['project_logo']['name']);
+    $uploadFile = $uploadDir . $target_name;
 
-        if (move_uploaded_file($_FILES['project_logo']['tmp_name'], $uploadFile)) {
-            $project_logo_path = '/storage/'. $target_name;
-        } else {
-            echo "File upload failed!";
-            exit();
-        }
-    } elseif (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] !== UPLOAD_ERR_NO_FILE) {
-        echo "File upload error occurred.";
-        exit();
+    if (move_uploaded_file($_FILES['project_logo']['tmp_name'], $uploadFile)) {
+      $project_logo_path = '/storage/logo/' . $target_name;
+    } else {
+      echo "File upload failed!";
+      exit();
     }
-    $sql = "
+  } elseif (isset($_FILES['project_logo']) && $_FILES['project_logo']['error'] !== UPLOAD_ERR_NO_FILE) {
+    echo "File upload error occurred.";
+    exit();
+  }
+  $sql = "
     INSERT INTO Projects (
         focal_person_id, project_name ,project_code,project_logo
     ) VALUES (
         :focal_person_id, :project_name, :project_code, :project_logo
     );";
-    
-    $id = execute("SELECT personnel_id FROM Personnels WHERE name = :n", ['n' => $_POST['focal_person']])->fetchColumn();
-    if (!$id) $id = null;
 
-    $args = [
-        ':focal_person_id' => $id,
-        ':project_name' => $_POST['project_name'],
-        ':project_code' => $_POST['project_code'],
-        ':project_logo' => $project_logo_path
-    ];
-    
-    execute($sql,$args);
-    
-    header('Location: /admin/projects.php');
-    exit();
-    
-    }
+  $id = execute("SELECT personnel_id FROM Personnels WHERE name = :n", ['n' => $_POST['focal_person']])->fetchColumn();
+  if (!$id) $id = null;
 
-    $sql = "
+  $args = [
+    ':focal_person_id' => $id,
+    ':project_name' => $_POST['project_name'],
+    ':project_code' => $_POST['project_code'],
+    ':project_logo' => $project_logo_path
+  ];
+
+  execute($sql, $args);
+
+  header('Location: /admin/projects.php');
+  exit();
+}
+
+$sql = "
     SELECT
         proj.*,
         pa.name
@@ -53,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     WHERE proj.deleted = false
     ";
 $stmt = execute($sql);
-$project = $stmt->fetchAll(); 
+$project = $stmt->fetchAll();
 
 $stmt = execute('SELECT name FROM Personnels');
 $personnels = $stmt->fetchAll();
@@ -62,5 +61,4 @@ $options = array_map(function ($item) {
 }, $personnels);
 
 
-echo $twig->render('projects.twig', ['options'=> $options,'project' => $project, 'count' => count($project)]);
-
+echo $twig->render('projects.twig', ['options' => $options, 'project' => $project, 'count' => count($project)]);
