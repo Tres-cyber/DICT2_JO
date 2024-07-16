8f2aac848f43a763fce82582a5cdd8fba1f705c5
wabbit12 <107483377+wabbit12@users.noreply.github.com>
2024-07-16 08:47:16 +0800

    updated projects table sql/added active page in pagination (#37)
    * migrated admin-job orders
    
    * fixed pagination/added pagination and search in personnels
    
    * updated projects table sql/added active page in pagination

---
+++ database/14-updated-projects-table.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    `Projects` (
+        `project_id`,
+        `project_name`,
+        `project_code`,
+        `focal_person_id`,
+        `project_logo`,
+        `deleted`
+    )
+VALUES
+    (
+        NULL,
+        'Cybersecurity',
+        'Cybersecurity',
+        '35',
+        '/storage/cybersecurity.png',
+        '0'
+    );
